54b41e17dd5d9be9a6821a5039dafd1dd4802d02
tarunsingh7379 <tarunsinghofficial236@gmail.com>
2021-12-23 16:00:09 +0530

    Updated Project

--- src/data.js
+++ src/data.js
@@ -4,31 +4,31 @@ export const products = [
   {
     id: 1,
     img: { img1 },
-    link: "https:tarun-singh.netlify.app",
+    link: "https://tarun-singh.netlify.app/",
   },
   {
     id: 2,
     img: { img1 },
-    link: "https:tarun-singh.netlify.app",
+    link: "https://tarun-singh.netlify.app/",
   },
   {
     id: 3,
     img: { img1 },
-    link: "https:tarun-singh.netlify.app",
+    link: "https://tarun-singh.netlify.app/",
   },
   {
     id: 4,
     img: { img1 },
-    link: "https:tarun-singh.netlify.app",
+    link: "https://tarun-singh.netlify.app/",
   },
   {
     id: 5,
     img: { img1 },
-    link: "https:tarun-singh.netlify.app",
+    link: "https://tarun-singh.netlify.app/",
   },
   {
     id: 6,
     img: { img1 },
-    link: "https:tarun-singh.netlify.app",
+    link: "https://tarun-singh.netlify.app/",
   },
 ];
